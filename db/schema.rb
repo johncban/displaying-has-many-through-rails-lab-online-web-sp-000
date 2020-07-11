26457f4b42bbb88c221b7b0c377c2a84a35c6c82
Juan Carlo Banayo <johncban@gmail.com>
2020-07-11 10:39:12 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200711101807) do
-
-  create_table "doctors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "department"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20200711101719) do
 
   create_table "patients", force: :cascade do |t|
     t.string   "name"
